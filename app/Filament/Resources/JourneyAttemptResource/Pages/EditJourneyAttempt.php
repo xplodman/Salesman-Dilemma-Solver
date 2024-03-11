bad1bba2012abff1fe6d0592585361e7f71fc2e7
ahmed <a.elsayed.it@gmail.com>
2024-03-11 15:20:02 +0200

    PHPCS

--- app/Filament/Resources/JourneyAttemptResource/Pages/EditJourneyAttempt.php
+++ app/Filament/Resources/JourneyAttemptResource/Pages/EditJourneyAttempt.php
@@ -17,36 +17,36 @@ class EditJourneyAttempt extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
-            Actions\Action::make( 'calculateShortestPath' )
-                          ->label( function ( JourneyAttempt $journeyAttempt ) {
-                              if ( $journeyAttempt->calculated ) {
-                                  return 'Recalculate';
-                              }
+            Actions\Action::make('calculateShortestPath')
+                          ->label(function (JourneyAttempt $journeyAttempt) {
+                            if ($journeyAttempt->calculated) {
+                                return 'Recalculate';
+                            }
 
                               return 'Calculate';
-                          } )
-                          ->color( function ( JourneyAttempt $journeyAttempt ) {
-                              if ( $journeyAttempt->calculated ) {
-                                  return 'info';
-                              }
+                          })
+                          ->color(function (JourneyAttempt $journeyAttempt) {
+                            if ($journeyAttempt->calculated) {
+                                return 'info';
+                            }
 
                               return 'success';
-                          } )
-                          ->icon( 'heroicon-s-arrow-path' )
+                          })
+                          ->icon('heroicon-s-arrow-path')
                           ->requiresConfirmation()
-                          ->action( function ( JourneyAttempt $journeyAttempt ) {
-                              if ( empty($journeyAttempt->startWaypoint)) {
-                                  // If start_point is not selected, redirect to the edit page with a message
+                          ->action(function (JourneyAttempt $journeyAttempt) {
+                            if (empty($journeyAttempt->startWaypoint)) {
+                                // If start_point is not selected, redirect to the edit page with a message
 
-                                  Notification::make()
-                                              ->title('Calculation Failed')
-                                              ->danger()
-                                              ->body('Please select a start waypoint before proceeding with the calculation.')
-                                              ->send();
-                                  return;
-                              }
+                                Notification::make()
+                                            ->title('Calculation Failed')
+                                            ->danger()
+                                            ->body('Please select a start waypoint before proceeding with the calculation.')
+                                            ->send();
+                                return;
+                            }
 
-                              $this->calculateShortestPath( $journeyAttempt );
+                              $this->calculateShortestPath($journeyAttempt);
 
                               // Display a success notification after the calculation is completed
                               Notification::make()
@@ -56,16 +56,17 @@ class EditJourneyAttempt extends EditRecord
                                           ->send();
 
                               // Redirect page to any URL you want
-                              redirect( static::getUrl( [
+                              redirect(static::getUrl([
                                   'name'   => 'edit', // Assuming 'name' is the correct key for the page/route name, adjust as necessary
                                   'record' => $journeyAttempt->id, // The actual parameters expected by the route
-                              ] ) );
-                          } ),
+                              ]));
+                          }),
         ];
     }
 
-    public function calculateShortestPath( JourneyAttempt $journeyAttempt ): void {
+    public function calculateShortestPath(JourneyAttempt $journeyAttempt): void
+    {
         $journeyRouteCalculator = new JourneyRouteCalculatorService();
-        $journeyRouteCalculator->calculateJourneyRoutes( $journeyAttempt );
+        $journeyRouteCalculator->calculateJourneyRoutes($journeyAttempt);
     }
 }
